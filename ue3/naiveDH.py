548fef461b1df8d15279d495dfa27bf56c860154
Steven Maasch <steven.maasch@gmx.de>
2014-12-16 01:12:51 +0100

    Key exchange

--- ue3/naiveDH.py
+++ ue3/naiveDH.py
@@ -16,10 +16,12 @@ IMAP_SERVER = {"url" : "imap.gmx.net", "port" : 993}
 SMTP_SERVER = {"url" : "mail.gmx.net", "port" : 587}
 
 USERNAME = ""
-PASSWORD = "
+PASSWORD = ""
 
 TO_ADDR = ""
 FROM_ADDR = ""
+
+SEPERATOR = "-" * 79
 
 def gen_dhke_key(base, exp, div):
 	return base ** exp % div
@@ -28,7 +30,7 @@ def send_mail(from_addr, to_addrs, msg):
 	server = smtplib.SMTP(SMTP_SERVER["url"], SMTP_SERVER["port"])
 	server.starttls()
 	server.ehlo()
-	server.login(username, password)
+	server.login(USERNAME, PASSWORD)
 	server.sendmail(from_addr, to_addrs, msg.as_string())
 	server.quit()
 
@@ -41,45 +43,44 @@ def create_mime_text_msg(from_addr, to_addr, subject, msg):
 
 def process_mailbox_for_keyexchange(mailbox):
 	res_select, count_of_messages = mailbox.select("INBOX", True)
-	res_search,  msgnums = mailbox.search(None, '(SUBJECT "Crypto")')
+	res_search,  msgnums = mailbox.search(None, '(SUBJECT "My Public Key")')
 	ids = msgnums[0]
 	id_list = ids.split()
 
 	if len(id_list) == 0:
 		print "No keyexchange mail found."
-
-	msgs = []
-
-	for mail_id in id_list:
-		res_fetch, data = mailbox.fetch(mail_id, "(RFC822)")
-		msgs.append(email.message_from_string(data[0][1]))
-
-	return msgs
+		return None
+	else:
+		res_fetch, data = mailbox.fetch(id_list[-1], "(RFC822)")
+		return email.message_from_string(data[0][1])
 
 def main():
 	logging.basicConfig(level=logging.DEBUG)
-	p = 49999
 	rand_gen = random.SystemRandom()
-	g = rand_gen.randrange(0, p)
-	print "p = {}".format(p)
-	print "g = {}".format(g)
+
+	print SEPERATOR
+	print "Alice"
+	print SEPERATOR
+
+	p = 23
+	g = 5
 	a = rand_gen.randrange(0, p)
-	print "a = {}".format(a)
-	public_key_a = gen_dhke_key(g, a, p)
-	print "Public key A = {}".format(public_key_a)
+	public_key = {"g" : g, "p" : p, "z" : gen_dhke_key(g, a, p)}
 
-	data_init_a = {"p" : p, "g" : g, "public_key" : public_key_a}
-	data_string = json.dumps(data_init_a)
-	print data_string
+	public_key_as_json_str = json.dumps(public_key)
+	print "Public key A (JSON) = {}".format(public_key_as_json_str)
+	public_key_b64 = base64.b64encode(public_key_as_json_str)
+	print "Public key A (base64) = {}".format(public_key_b64)
 
-	data_b64 = base64.b64encode(data_string)
-	print "Data (base64) = {}".format(data_b64)
-
-	msg = create_mime_text_msg(TO_ADDR, FROM_ADDR, "Crypto-Key", data_b64)
-
+	msg = create_mime_text_msg(TO_ADDR, FROM_ADDR, "My Public Key", public_key_b64)
 	send_mail(FROM_ADDR, [TO_ADDR], msg)
+	print "Send public key to {}".format(TO_ADDR)
 
 	time.sleep(10)
+
+	print SEPERATOR
+	print "Bob"
+	print SEPERATOR
 
 	mail = imaplib.IMAP4_SSL(IMAP_SERVER["url"], IMAP_SERVER["port"])
 	try:
@@ -87,31 +88,24 @@ def main():
 	except imaplib.IMAP4.error:
 		print "LOGIN FAILED!"
 
-	mails_b = process_mailbox_for_keyexchange(mail)
+	mail_b = process_mailbox_for_keyexchange(mail)
+	payload = base64.b64decode(mail_b.get_payload())
+	print "Public key (JSON) from {} = {}".format(mail_b.get("From"), payload)
+	public_key_a = json.loads(payload)
 
-	for msg in mails_b:
-		payload = base64.b64decode(msg.get_payload())
-		print payload
-		#return json.loads(payload)
-
-
-	print a_keys
-
-	b = rand_gen.randrange(0, a_keys["p"])
-	public_key_b = gen_dhke_key(a_keys["g"], b, a_keys["p"])
-
-	print "Key B = {}".format(gen_dhke_key(a_keys["public_key"], b, a_keys["p"]))
-
-	data_init_b = {"public_key" : public_key_b}
-	data_string_b = json.dumps(data_init_b)
-	print data_string_b
-
-	data_b64_b = base64.b64encode(data_string_b)
-
-	send_mail(FROM_ADDR, TO_ADDR, "Crypto-Key-Response", data_b64_b)
+	b = rand_gen.randrange(0, public_key_a["p"])
+	public_key_b = {"g" : g, "p" : p, "z" : gen_dhke_key(public_key_a["g"], b, public_key_a["p"])}
+	public_key_b_as_json_str = json.dumps(public_key_b)
+	print "Public Key B = {}".format(public_key_b_as_json_str)
+	public_key_b_b64 = base64.b64encode(public_key_b_as_json_str)
+	
+	msg_b = create_mime_text_msg(TO_ADDR, FROM_ADDR,
+		"Public Key Response", public_key_b_b64)
+	send_mail(FROM_ADDR, TO_ADDR, msg_b)
 
 	time.sleep(10)
 
+	# GO ON
 	b_key = process_key_response(mail)
 
 	mail.logout()
